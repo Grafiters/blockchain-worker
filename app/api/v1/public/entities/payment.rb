93b3ecf1c02be75656df8acda477b36dde906115
Grafiters <bayugrafit@gmail.com>
2023-02-08 14:51:35 +0700

    refac(payment): payment for payment method

---
+++ app/api/v1/public/entities/payment.rb
@@ -0,0 +1,67 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+module API
+    module V1
+      module Public
+        module Entities
+            class Payment < Grape::Entity
+                expose(
+                    :id,
+                    as: :id,
+                    documentation: {
+                        desc: 'Filter Fiat.',
+                        type: String
+                    }
+                )
+    
+                expose(
+                    :name,
+                    as: :bank_name,
+                    documentation: {
+                        desc: 'Filter Fiat.',
+                        type: String
+                    }
+                )
+    
+                expose(
+                    :symbol,
+                    as: :symbol,
+                    documentation: {
+                        desc: 'Filter Fiat.',
+                        type: String
+                    }
+                )
+    
+                expose(
+                    :logo_url,
+                    as: :logo,
+                    documentation: {
+                        desc: 'Filter Fiat.',
+                        type: String
+                    }
+                )
+    
+                expose(
+                    :base_color,
+                    as: :base_color,
+                    documentation: {
+                        desc: 'Filter Fiat.',
+                        type: String
+                    }
+                )
+    
+                expose(
+                    :state,
+                    as: :state,
+                    documentation: {
+                        desc: 'Filter Fiat.',
+                        type: String
+                    }
+                )
+            end
+        end
+      end
+    end
+end
+  
