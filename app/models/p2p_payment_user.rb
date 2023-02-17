ba39a1751a68805081386147a0a7e8bc9cc7226d
Grafiters <bayugrafit@gmail.com>
2023-02-17 13:46:15 +0700

    refac(p2p_payment): add enumrize state bank or ewaller

--- app/models/p2p_payment_user.rb
+++ app/models/p2p_payment_user.rb
@@ -3,4 +3,22 @@ class P2pPaymentUser < ApplicationRecord
     
     belongs_to :p2p_users
     belongs_to :p2p_payment
+
+    after_commit on: :create do
+        assign_uuid
+    end
+
+    private
+    def assign_uuid
+        return unless payment_user_uid.blank?
+
+        self.payment_user_uid = InterIDGenerate('P4Y')
+    end
+
+    def InterIDGenerate(prefix)
+        loop do
+          uid = "%s%s" % [prefix.upcase, SecureRandom.hex(5).upcase]
+          return uid if P2pPaymentUser.where(payment_user_uid: uid).empty?
+        end
+    end
 end
