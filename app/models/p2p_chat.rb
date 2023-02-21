72c8a631caecfae26d6219fe9c1e0edabdf50235
Grafiters <bayugrafit@gmail.com>
2023-02-21 21:03:52 +0700

    refac(chat): add mountable uploader on confirmation chat

--- app/models/p2p_chat.rb
+++ app/models/p2p_chat.rb
@@ -1,3 +1,9 @@
 class P2pChat < ApplicationRecord
+    mount_uploader :upload, UploaderUploader
+
     belongs_to :p2p_order, dependent: :destroy
+
+    def verification_url
+        "/api/v2/p2p/confirmation_chat/#{id}"
+    end
 end
