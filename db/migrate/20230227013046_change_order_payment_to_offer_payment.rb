8b3741e504ca5c6e2ae76e6e4587566af1adeaa1
devops-nusatech <devops@nusatech.id>
2023-02-27 18:56:37 +0700

    refac(update): rename table on p2p order payment

--- db/migrate/20230227013046_change_order_payment_to_offer_payment.rb
+++ db/migrate/20230227013046_change_order_payment_to_offer_payment.rb
@@ -1,6 +1,5 @@
 class ChangeOrderPaymentToOfferPayment < ActiveRecord::Migration[5.2]
   def change
-    rename_table :p2p_offer_payments, :p2p_order_payments
     rename_table :p2p_order_payments, :p2p_offer_payments
 
     rename_column :p2p_orders, :p2p_order_payment_id, :p2p_payment_user_id
