f3556527e9e2e1f7e0a5590bee120f52acc9a244
devops-nusatech <devops@nusatech.id>
2023-02-28 10:18:31 +0700

    refac(merge): merge

--- app/uploaders/uploader_uploader.rb
+++ app/uploaders/uploader_uploader.rb
@@ -1,66 +1,66 @@
-class UploaderUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+# class UploaderUploader < CarrierWave::Uploader::Base
+#   # Include RMagick or MiniMagick support:
+#   # include CarrierWave::RMagick
+#   # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
+#   # Choose what kind of storage to use for this uploader:
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-    storage :file
+#   # Override the directory where uploaded files will be stored.
+#   # This is a sensible default for uploaders that are meant to be mounted:
+#     storage :file
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-#   def cache_dir
-#     "uploads/tmp/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+#   def store_dir
+#     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
 #   end
 
-  def move_to_store
-    true
-  end
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+# #   def cache_dir
+# #     "uploads/tmp/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+# #   end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-  def extension_whitelist
-    %w(jpg jpeg png)
-  end
+#   def move_to_store
+#     true
+#   end
+#   # Provide a default URL as a default if there hasn't been a file uploaded:
+#   # def default_url(*args)
+#   #   # For Rails 3.1+ asset pipeline compatibility:
+#   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+#   #
+#   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+#   # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
-  def size_range
-    # default is 1..10.megabytes
-    1..10.megabytes
-  end
-
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
-
-#   def fog_public
-#     false
+#   # Process files as they are uploaded:
+#   # process scale: [200, 300]
+#   #
+#   # def scale(width, height)
+#   #   # do something
+#   # end
+#   def extension_whitelist
+#     %w(jpg jpeg png)
 #   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+#   # Create different versions of your uploaded files:
+#   # version :thumb do
+#   #   process resize_to_fit: [50, 50]
+#   # end
+#   def size_range
+#     # default is 1..10.megabytes
+#     1..10.megabytes
+#   end
 
-end
+#   # Add an allowlist of extensions which are allowed to be uploaded.
+#   # For images you might use something like this:
+#   # def extension_allowlist
+#   #   %w(jpg jpeg gif png)
+#   # end
+
+# #   def fog_public
+# #     false
+# #   end
+
+#   # Override the filename of the uploaded files:
+#   # Avoid using model.id or version_name here, see uploader/store.rb for details.
+#   # def filename
+#   #   "something.jpg" if original_filename
+#   # end
+
+# end
--- config/initializers/carierwave.rb
+++ config/initializers/carierwave.rb
@@ -1,13 +1,13 @@
 # # frozen_string_literal: true
 
-require 'carrierwave/storage/abstract'
-require 'carrierwave'
-require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave'
+# require 'carrierwave/storage/file'
+# # require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-    config.storage = :file
-    config.permissions = 0666
-    config.directory_permissions = 0777
-    config.store_dir = "uploads/images/"
-end
+# CarrierWave.configure do |config|
+#     config.storage = :file
+#     config.permissions = 0666
+#     config.directory_permissions = 0777
+#     config.store_dir = "uploads/images/"
+# end
