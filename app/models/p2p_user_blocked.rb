d849d2984ba99a63a9746edd38e878d55ddb2b03
Grafiters <bayugrafit@gmail.com>
2023-02-28 10:12:52 +0700

    refac(blocked): update data

--- app/models/p2p_user_blocked.rb
+++ app/models/p2p_user_blocked.rb
@@ -1,6 +1,6 @@
 class P2pUserBlocked < ApplicationRecord
     belongs_to :p2p_user, class_name: "P2pUser", foreign_key: :p2p_user_id, primary_key: :id
-    belongs_to :target_user, class_name: "P2pUser", foreign_key: :p2p_user_id, primary_key: :id
+    belongs_to :target_user, class_name: "P2pUser", foreign_key: :target_user_id, primary_key: :id
 
     extend Enumerize
     STATES = { unblocked: 100, blocked: -100 }
