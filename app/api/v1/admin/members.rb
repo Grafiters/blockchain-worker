2fe723b7d99922d59e20f706b61a9504bc5cb44f
Grafiters <bayugrafit@gmail.com>
2023-03-10 18:03:39 +0700

    feat(member): list member user p2p

--- app/api/v1/admin/members.rb
+++ app/api/v1/admin/members.rb
@@ -11,33 +11,51 @@ module API
             is_array: true,
             success: API::V2::Admin::Entities::Member
           params do
-            optional :state,
-                     desc: 'Filter order by state.'
-            optional :role,
-                     values: { value: -> { ::Ability.roles }, message: 'admin.member.invalid_role' }
-            optional :group,
-                     values: { value: -> { ::Member.groups }, message: 'admin.member.invalid_group' }
             optional :email,
-                     desc: -> { API::V2::Entities::Member.documentation[:email][:desc] }
+                    desc: -> { API::V2::Entities::Member.documentation[:email][:desc] }
+            optional :username,
+                    desc: -> { API::V2::Entities::Member.documentation[:username][:desc] }
+            optional :p2p_name,
+                    desc: -> { API::V1::Entities::UserP2p.documentation[:username][:desc] }
             use :uid
-            use :date_picker
-            use :pagination
-            use :ordering
           end
-          get '/members' do
-            admin_authorize! :read, ::Member
-  
-            ransack_params = Helpers::RansackBuilder.new(params)
-                               .eq(:uid, :email, :state, :role, :group)
-                               .with_daterange
-                               .build
-  
-            search = Member.ransack(ransack_params)
-            search.sorts = "#{params[:order_by]} #{params[:ordering]}"
-            present paginate(search.result), with: API::V2::Admin::Entities::Member
+          get '/members' do  
+            search = P2pUser.joins(:member).order(id: :desc)
+                      .tap { |q| q.where!(members: {uid: params[:uid]}) if params[:uid].present? }
+                      .tap { |q| q.where!(members: {email: params[:email]}) if params[:email].present? }
+                      .tap { |q| q.where!(members: {username: params[:username]}) if params[:username].present? }
+                      .tap { |q| q.where!(p2p_users: {username: params[:p2p_name]}) if params[:p2p_name].present? }
+                      .tap { |q| present paginate(search), with: API::V1::Entities::UserP2p }
+          end
+
+          desc 'Desc Detail Of User Member p2p'
+          get '/:uid' do
+            present P2pUser.joins(:member).find_by(uid: params[:uid]), with: API::V1::Entities::UserP2p
+          end
+
+          desc 'banned user p2p'
+          params do
+            requires :banned_time,
+                   type: { value: Time, message: 'admin.filter.range_from_invalid' },
+                   desc: 'An integer represents the seconds elapsed since Unix epoch.'\
+                     'If set, only entities FROM the time will be retrieved.'
+          end
+          post '/:uid/banned' do
+            member = P2pUser.joins(:member).find_by(members: { uid: params[:uid]})
+
+            P2pOrder.where('maker_uid = ? OR taker_uid = ?', params[:uid], params[:uid]).each do |order|
+              order.update!(state: 'canceled')
+              return error!({ errors: ['admin.p2p_order.can_not_send_message_order_is_done'] }, 422) unless order?
+            end
+
+            P2pOffer.where(p2p_user_id: member[:id]).each do |offer|
+              offer.update!(state: 'canceled')
+              return error!({ errors: ['admin.p2p_order.can_not_send_message_order_is_done'] }, 422) unless offer?
+            end
+
+            member.update(banned_time: Time.at(params[:banned_time]))
           end
         end
       end
     end
-  end
-  
+end
