f1d550917dd05450dfee0c4d68d6429aa6ce6d5c
Grafiters <bayugrafit@gmail.com>
2024-02-13 10:40:01 +0700

    refac(migration): rename table target

--- db/migrate/20240213033104_add_blockchain_key_on_spesific_folder.rb
+++ db/migrate/20240213033104_add_blockchain_key_on_spesific_folder.rb
@@ -1,6 +1,6 @@
 class AddBlockchainKeyOnSpesificFolder < ActiveRecord::Migration[5.2]
   def change
-    add_column :payment_address, :blockchain_key, :string, limit: 32, after: :id
+    add_column :payment_addresses, :blockchain_key, :string, limit: 32, after: :id
     add_column :withdraws, :blockchain_key, :string, limit: 32, after: :id
     add_column :deposits, :blockchain_key, :string, limit: 32, after: :id
   end
